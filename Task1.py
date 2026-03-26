df4cc3607ea363653e916b1d2d74d3c921e02183
Abishaan-G <s27437@caseygrammar.vic.edu.au>
2026-03-26 00:42:06 +0000

    Completed Task 1

--- Task1.py
+++ Task1.py
@@ -2,11 +2,12 @@
 # Task 1  Input: Hello
 def main():
     # Write your code for input here
-    
     var = input('What is your name: ')
     print(f'Hi, {var}')
 
     # End of your code for input here
+task1 = input('Hello')
+    
 
 
 
